63e0a6793da4a07a9044d086b256a3dd70d72e38
koki-develop <kou.pg.0131@gmail.com>
2022-12-11 14:57:41 +0900

    https 対応

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -12,14 +12,36 @@ resource "google_compute_url_map" "main" {
   default_service = google_compute_backend_bucket.main.id
 }
 
+resource "google_compute_managed_ssl_certificate" "main" {
+  name = "${local.name}-cert"
+  managed {
+    domains = [var.domain]
+  }
+}
+
 resource "google_compute_target_http_proxy" "main" {
   name    = "${local.name}-http"
   url_map = google_compute_url_map.main.id
+}
+
+resource "google_compute_target_https_proxy" "main" {
+  name    = "${local.name}-https"
+  url_map = google_compute_url_map.main.id
+  ssl_certificates = [
+    google_compute_managed_ssl_certificate.main.id
+  ]
 }
 
 resource "google_compute_global_forwarding_rule" "http" {
   name       = "${local.name}-lb-http"
   port_range = "80"
   target     = google_compute_target_http_proxy.main.id
+  ip_address = google_compute_global_address.main.id
+}
+
+resource "google_compute_global_forwarding_rule" "https" {
+  name       = "${local.name}-lb-https"
+  port_range = "443"
+  target     = google_compute_target_https_proxy.main.id
   ip_address = google_compute_global_address.main.id
 }
